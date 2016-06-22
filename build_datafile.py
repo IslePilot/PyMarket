6735dcf01a0523cec5312ea10226dcedf7550bb1
Keith Barr <Keith Barr>
2016-06-21 21:36:13 -0600

    WIP

--- build_datafile.py
+++ build_datafile.py
@@ -9,50 +9,78 @@ import datetime
 
 from yahoo_finance import Share
 
-class FinancialData:
-  def __init__(self, date):
-    self.date = date
 
-    # initialize the S&P500 Data
+def daterange( start_date, end_date ):
+    if start_date <= end_date:
+        for n in range( ( end_date - start_date ).days + 1 ):
+            yield start_date + datetime.timedelta( n )
+    else:
+        for n in range( ( start_date - end_date ).days + 1 ):
+            yield start_date - datetime.timedelta( n )
+ 
 
-
-start_date = '2015-06-01'
-end_date = datetime.datetime.now().strftime("%Y-%m-%d")
-
+def hist_to_csv(filename, history,dividends=None):
+  # open the file
+  outfile = open(filename, 'w')
+  
+  # add labels
+  outfile.write("date,open,close,adj_close,high,low,volume,dividend\n")
+  
+  for daily_data in history:
+      string = "%s,"%daily_data['Date']
+      string+="%s,"%daily_data['Open']
+      string+="%s,"%daily_data['Close']
+      string+="%s,"%daily_data['Adj_Close']
+      string+="%s,"%daily_data['High']
+      string+="%s,"%daily_data['Low']
+      string+="%s,"%daily_data['Volume']
+      
+      # look through the dividend list for a dividend for this date
+      dividend = 0.0
+      
+      if dividends != None:
+          for div in dividends:
+              if div['Date'] == history['Date']:
+                  dividend = div['Dividends']
+              
+      string+="%s\n"%dividend
+      
+      outfile.write(string)
+  
+  # close the file
+  outfile.close()
+    
+start_date = datetime.date(year=2015, month=1, day = 1)
+end_date = datetime.date.today()
 print "Processing from %s to %s\n"%(start_date, end_date)
 
+start_string = start_date.strftime("%Y-%m-%d")
+end_string = end_date.strftime("%Y-%m-%d")
+
 # get the stock data
-sp500 = list(reversed(Share('^GSPC').get_historical(start_date, end_date)))
-nasdaq = list(reversed(Share('^IXIC').get_historical(start_date, end_date)))
+path = "C:\\py_scripts\\PyMarket"
+sp500 = list(reversed(Share('^GSPC').get_historical(start_string, end_string)))
+hist_to_csv("%s\\sp500.csv"%path, sp500)
 
-ivv = list(reversed(Share('IVV').get_historical(start_date, end_date)))
-qqq = list(reversed(Share('QQQ').get_historical(start_date, end_date)))
+ivv = list(reversed(Share('IVV').get_historical(start_string, end_string)))
+ivv_dividends = list(reversed(Share('IVV').get_dividend_history(start_string, end_string)))
+hist_to_csv("%s\\ivv.csv"%path, ivv, ivv_dividends)
 
-sh = list(reversed(Share('SH').get_historical(start_date, end_date)))
+sh = list(reversed(Share('SH').get_historical(start_string, end_string)))
+sh_dividends = list(reversed(Share('SH').get_dividend_history(start_string, end_string)))
+hist_to_csv("%s\\sh.csv"%path, sh, sh_dividends)
 
-# get the dividend data
-ivv_dividends = list(reversed(Share('IVV').get_dividend_history(start_date, end_date)))
-sh_dividends = list(reversed(Share('SH').get_dividend_history(start_date, end_date)))
+"""
+nasdaq = list(reversed(Share('^IXIC').get_historical(start_string, end_string)))
+qqq = list(reversed(Share('QQQ').get_historical(start_string, end_string)))
+qqq_dividends = list(reversed(Share('QQQ').get_dividend_history(start_string, end_string)))
+psq = list(reversed(Share('PSQ').get_historical(start_string, end_string)))
+psq_dividends = list(reversed(Share('PSQ').get_dividend_history(start_string, end_string)))
+
+# also try QID, SQQQ
 
 # we need to combine the data into a complete lis
+for date in daterange(start_date, end_date):
+    print date
+"""
 
-
-# open the file
-filename = open(r"C:\py_scripts\pyMarket\MarketData_%s_%s.csv"%(start_date, end_date), 'w')
-
-# add a header
-filename.write("Date,Open,Close,Adj_Close,High,Low,Volume\n")
-for day in sp500:
-  # put the data in the csv file
-  string="%s,"%day['Date']
-  string+="%s,"%day['Open']
-  string+="%s,"%day['Close']
-  string+="%s,"%day['Adj_Close']
-  string+="%s,"%day['High']
-  string+="%s,"%day['Low']
-  string+="%s,"%day['Volume']
-  string+="\n"
-
-  filename.write(string)
-
-filename.close()
